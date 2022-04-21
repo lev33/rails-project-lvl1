1fbaf4dd16e3fdfbbfc6ac54daf177801d043353
lev33 <48180783+lev33@users.noreply.github.com>
2022-04-21 08:14:11 +0300

    add Tag.build

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -5,4 +5,19 @@ require_relative "hexlet_code/version"
 module HexletCode
   class Error < StandardError; end
   # Your code goes here...
+  class Tag
+    def self.build(name, **attributes)
+      attributes = attributes.map do |key, value|
+        %( #{key}='#{value}')
+      end.join
+      
+      if %w(br img input).include?(name)
+        %(<#{name}#{attributes}>)
+      elsif block_given?
+        %(<#{name}#{attributes}>#{yield}</#{name}>)
+      else
+        %(<#{name}#{attributes}></#{name}>)
+      end
+    end
+  end
 end
