883b78adba495969dd4fbac6cbe517c5a7c82693
lev33 <48180783+lev33@users.noreply.github.com>
2022-04-27 18:25:26 +0300

    add tests

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -6,15 +6,37 @@ require_relative "hexlet_code/version"
 module HexletCode
   class Error < StandardError; end
 
-  def self.form_for(_user, url: "#")
-    Tag.build("form", action: url, method: "post") {}
+  def self.form_for(user, url: "#")
+    Tag.build("form", action: url, method: "post") do
+      yield(Form.new(user)) if block_given?
+    end
+  end
+
+  # documentation comment
+  class Form
+    attr_reader :user
+
+    def initialize(user)
+      @user = user
+      @html = ""
+    end
+
+    def input(name, **attributes)
+      value = user.send name
+      @html += case attributes[:as]
+               when :text
+                 Tag.build("textarea", cols: "20", rows: "40", name: name) { value }
+               else
+                 Tag.build("input", name: name, type: "text", value: value)
+               end
+    end
   end
 
   # documentation comment
   class Tag
     def self.build(name, **attributes)
       attributes = attributes.map do |key, value|
-        %( #{key}='#{value}')
+        %( #{key}="#{value}")
       end.join
 
       if %w[br img input].include?(name)
--- test/test_hexlet_code.rb
+++ test/test_hexlet_code.rb
@@ -2,12 +2,37 @@
 
 require "test_helper"
 
+User = Struct.new(:name, :job, :gender, keyword_init: true)
+
 class TestHexletCode < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::HexletCode::VERSION
   end
 
   def test_it_does_something_useful
-    assert false
+    expected = File.read("test/fixtures/test_it_does_something_useful.html")
+    pp expected
+    user = User.new name: "rob", job: "hexlet", gender: "m"
+    result = HexletCode.form_for user do |f|
+      # Проверяет есть ли значение внутри name
+      f.input :name
+      # Проверяет есть ли значение внутри job
+      f.input :job, as: :text
+    end
+    pp result
+    assert expected == result
+  end
+
+  def test_field_does_not_exist_error
+    user = User.new job: "hexlet"
+    assert_raises NoMethodError do
+      HexletCode.form_for user, url: "/users" do |f|
+        f.input :name
+        f.input :job, as: :text
+        # Поля age у пользователя нет
+        f.input :age
+        f.submit
+      end
+    end
   end
 end
